2c504c2b24626ab993f469bf6443993f3d047800
KoychoKoychev <kgkoychev@gmail.com>
2022-01-29 14:09:28 +0200

    fix: wrap the whole Book component in anchor

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -2,16 +2,16 @@ import "./Book.css"
 
 export default function Book({ _id, imgUrl, title, authorName }) {
     return (
-        <div className="book-cont">
-            <div className="img-cont">
-                <img className="book-cover" src={imgUrl} alt="book-tumbnail" />
-            </div>
-            <a href={`/listen:${_id}`}>
+        <a href={`/listen:${_id}`}>
+            <div className="book-cont">
+                <div className="img-cont">
+                    <img className="book-cover" src={imgUrl} alt="book-tumbnail" />
+                </div>
                 <div className="book-info">
                     <p className="book-title">{title}</p>
                     <p className="book-author">{authorName}</p>
                 </div>
-            </a>
-        </div>
+            </div>
+        </a>
     )
 }
